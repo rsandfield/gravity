5a3f9da8c3c0f1039ff061bc99ec733803d1dd55
Robert Sandfield <robert.f.sandfield@gmail.com>
2026-01-06 15:05:37 -0800

    Add rigid body interaction to character controller

--- demo/character_controller.gd
+++ demo/character_controller.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+const MINIMUM_PUSHABLE_MASS_RATIO := 0.25
+const PUSH_FORCE_MULTIPLIER := 5.0
+
+@export var mass = 80
 @export var walk_speed = 3
 @export var ground_acceleration := 4.0
 @export var ground_friction := 2.5
@@ -34,6 +38,7 @@ func _physics_process(delta: float):
 	if _jumping:
 		velocity += up_direction * jump_velocity
 		_jumping = false
+	_push_away_rigid_bodies()
 	move_and_slide()
 
 
@@ -62,7 +67,7 @@ func _handle_mouse_input(event: InputEvent):
 		_head.rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		_head.rotation_degrees.x -= event.relative.y * mouse_sensitivity
 		_head.rotation.y = fposmod(_head.rotation.y, TAU)
-		_head.rotation.x = clamp(_head.rotation.x, -PI / 2, PI / 2)
+		_head.rotation.x = clamp(_head.rotation.x, -PI / 2 + 0.1, PI / 4)
 
 	if event is InputEventMouseButton:
 		_shoot()
@@ -75,12 +80,37 @@ func _shoot():
 	projectile.global_position += _head.global_basis.z * -3
 	projectile.linear_velocity = _head.global_basis.z.normalized() * -50
 
+
 func move(dir: Vector2):
 	var right_dir = _head.global_basis.x
 	var forward_dir = _head.global_basis.z
 
 	var raw_dir = (forward_dir * dir.y + right_dir * dir.x).normalized()
 	_wish_dir = (raw_dir - up_direction * raw_dir.dot(up_direction)).normalized()
+
+
+func _push_away_rigid_bodies():
+	for i in get_slide_collision_count():
+		var c := get_slide_collision(i)
+		var collided = c.get_collider()
+		if collided is RigidBody3D:
+			var push_dir = -c.get_normal()
+			# How much velocity the object needs to increase to match player velocity in the
+			# push direction
+			var velocity_diff_in_push_dir = (
+				velocity.dot(push_dir) - collided.linear_velocity.dot(push_dir)
+			)
+			# No diff needed if moving away
+			velocity_diff_in_push_dir = max(0.0, velocity_diff_in_push_dir)
+			var mass_ratio = min(1.0, mass / collided.mass)
+			if mass_ratio < MINIMUM_PUSHABLE_MASS_RATIO:
+				continue
+			push_dir.y = 0
+			var push_force = mass_ratio * PUSH_FORCE_MULTIPLIER
+			collided.apply_impulse(
+				push_dir * velocity_diff_in_push_dir * push_force,
+				c.get_position() - collided.global_position
+			)
 
 
 func get_ground_speed() -> float:
